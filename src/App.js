151e68b581288c17dc7ec55fe3d09b6ee5a9e666
caleb-ali <slimkally@gmail.com>
2022-10-30 21:38:27 +0100

    finished setting up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import profileimg from "./images/profileimg.jpg";
+import "./input.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="grid place-items-center pt-8 pr-2 pb-2 pl-2 ">
+      <div class="pt-4" id= "profile__img">
+        <img class="h-24 w-24 rounded-full " src={profileimg} alt="" />
+      </div>
+      <p class="pt-4">Caleb Ali</p>
+      
+      <a class="" href="https://twitter.com/GodfatherDev" target="_blank" rel="noreferrer">Twitter link</a>
+      <a class="" href="" target="_blank" rel="noreferrer">hidden slack</a>
+      <a class="" href="" target="_blank" rel="noreferrer">Zuri Team</a>
+      <a class="" href="" target="_blank" rel="noreferrer">Zuri Books</a>
+      <a class="" href="" target="_blank" rel="noreferrer">Python Books</a>
+      <a class="" href="" target="_blank" rel="noreferrer">Background Check for Coders</a>
+      <a class="" href="" target="_blank" rel="noreferrer">Design Books</a>
+      
     </div>
   );
-}
+};
 
 export default App;
