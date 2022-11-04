852ea816d15e08e8ba30ff8ed9c4a9fca529a304
caleb-ali <slimkally@gmail.com>
2022-11-04 15:01:47 +0100

    refactor: created components and routes

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./input.css";
+import footer from "./images/Footerfooter.png";
+
+const Footer = () => {
+  return (
+    
+     
+      <div class=" mt-28 ">
+      <img class=" " src={footer} alt="" />
+      </div>
+  )
+}
+
+export default Footer;
---
+++ src/routes/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Frontpage from "../components/Frontpage";
+import Footer from "../components/Footer"
+
+
+const Home = () => {
+  return (
+    <div>
+    <Frontpage/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default Home;
