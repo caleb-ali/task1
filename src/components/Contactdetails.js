a855f2a4bba586a186a8cd1357a1252e63cea9ca
caleb-ali <slimkally@gmail.com>
2022-11-05 23:34:08 +0100

    style: styled contact and error page

--- src/components/Contactdetails.js
+++ src/components/Contactdetails.js
@@ -2,32 +2,63 @@ import React from "react";
 
 const Contactdetails = () => {
   return (
-    <div class="py-8 lg:py-16 px-32 mx-auto max-w-screen-md">
+    <div class="py-4 lg:py-16 px-32 mx-auto max-w-screen-md">
       <h1 class="mb-4 text-4xl tracking-tight font-bold text-left" s>
         Contact Me
       </h1>
       <p>Hi there, contact me to ask about anything you have in mind.</p>
-      <div class="md:flex items-center mt-12">
-        <div>
-          <label>First Name</label>
-          <input></input>
+      <div class="md:flex items-center gap-4 mt-12">
+        <div class="">
+          <label class="font-semibold  text-gray-700">First name</label>
+          <input
+            id="first_name"
+            class=" w-full  text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-white border rounded border-gray-300 placeholder-gray-500"
+            placeholder="Enter your first name"
+          />
         </div>
-        <div>
-          <label>Last name</label>
-          <input></input>
+        <div class="">
+          <label class="font-semibold text-gray-700">Last name</label>
+          <input
+            id="last_name"
+            class=" w-full text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-white border rounded border-gray-300 placeholder-gray-500"
+            placeholder="Enter your last name"
+          />
         </div>
       </div>
+      <div class=" mt-4">
+        <label class="font-semibold  text-gray-700">Email</label>
+        <input
+          id="email"
+          class="  w-full text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-white mt-4 bg-white border rounded border-gray-300 placeholder-gray-500"
+          placeholder="yourname@email.com"
+          required
+        />
+      </div>
       <div>
-        <label>
-        Email
+        <div className="w-full flex flex-col mt-4">
+          <label class="font-semibold  text-gray-700">Message</label>
+          <textarea
+            id="message"
+            className="h-36 leading-none text-gray-900 p-3  focus:border-sky-300 mt-4  bg-white border rounded border-gray-300 placeholder-gray-500"
+            placeholder="Send me a message and I'll reply as soon as possible..."
+          />
+        </div>
+      </div>
+      <div class="mt-4">
+        <label class="inline-flex items-center">
+          <input type="checkbox" class="w-4 h-4 rounded text-gray-900" />
+          <span class="ml-2 text-gray-500">
+            You agree to providing your data to caleb who may contact you{" "}
+          </span>
         </label>
-        <input></input>
       </div>
-      <div>
-        <label>Message</label>
-        <textarea></textarea>
-      </div>
-      <button></button>
+      <button
+        type="submit"
+        id="btn__submit"
+        class=" w-full bg-blue-500 hover:bg-blue-700 text-white font-semibold mt-8  py-3 px-4  rounded"
+      >
+        <span class="">Send message </span>
+      </button>
     </div>
   );
 };
--- src/components/Errorpage.js
+++ src/components/Errorpage.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React from "react";
+import iconleft from "../images/Iconleft.png";
+
+import {useNavigate } from "react-router-dom";
+  
+
+
+
+
 
 const Errorpage = () => {
+
+
+    const navigate = useNavigate();
+
+  const handleClick = () => {
+    // 👇️ navigate programmatically
+    navigate('/');
+  };
+
+    
   return (
-    <div>
-        <h4>404 error</h4>
-        <h1>We cant find that page </h1>
-        <p> Sorry,the page you are looking for doesn't exist.</p>
-        <div>
-            <button></button>
-            <button></button>
-        </div>
+    <div class="pt-40 pr-4 pb-32 pl-2 xl:pr-60 xl:pl-28">
+      <h4 class="text-blue-600 text-base">404 error</h4>
+      <h1 class="text-gray-900 font-semibold text-6xl mt-4">
+        We cant find that page{" "}
+      </h1>
+      <p class="text-gray-600 text-xl mt-6">
+        {" "}
+        Sorry, the page you are looking for doesn't exist.
+      </p>
+      <div class="mt-12 ">
+        <button
+          type="submit"
+          class=" bg-white hover:bg-gray-100 text-gray-700 border-gray-400 font-semibold py-3 px-4  border border-gray-400  rounded inline-flex  space-x-4 items-center" onClick={handleClick}
+        >
+         
+          <img class="w-4 h-4 " src={iconleft} alt="" />
+          <span>Go back</span>
+        </button>
+        <button
+          type="submit"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-semibold  ml-6 py-3 px-4  rounded" onClick={handleClick}
+        >
+         <span class="">take me home </span>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Errorpage;
